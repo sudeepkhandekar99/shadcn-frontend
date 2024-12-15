d1f4fc9e704f8d89db53e7981dd9312befa8bbfd
Sudeep Khandekar <sudeepkhandekar99@gmail.com>
2024-12-15 01:46:40 -0500

    Implemented Cards

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav>
+          <h1>Recipes for Ninjas</h1>
+        </nav>
         {children}
       </body>
     </html>
