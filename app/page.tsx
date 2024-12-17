eb1a621edab6615f4236d75371f3944cb4933249
Sudeep Khandekar <sudeepkhandekar99@gmail.com>
2024-12-17 06:54:15 -0500

    Added Cards with their Skeletons

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-
+import { 
+  Card, 
+  CardContent, 
+  CardDescription, 
+  CardFooter, 
+  CardHeader, 
+  CardTitle 
+} from '@/components/ui/card'
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { resolve } from 'path'
 
 interface Recipe {
   title: string,
@@ -17,34 +20,45 @@ interface Recipe {
   id: string
 }
 
-async function  getRecipes(): Promise<Recipe[]>{
+async function getRecipes(): Promise<Recipe[]> {
   const result = await fetch('http://localhost:4000/recipes')
-  console.log(result)
+
+  // delay response
+  await new Promise((resolve) => setTimeout(resolve,3000))
+
   return result.json()
 }
 
 export default async function Home() {
   const recipes = await getRecipes()
+
   return (
-    <div className="grid grid-cols-3 gap-8">
-      {recipes.map(recipe => (
-        <Card key={recipe.id} className="flex flex-col justify-between">
-          <CardHeader className="flex-row gap-4 items-center">
-            {/* avatar */}
-            <div>
-              <CardTitle>{recipe.title}</CardTitle>
-              <CardDescription>{recipe.time} mins to cook.</CardDescription>
-            </div>
-          </CardHeader>
-          <CardContent>
-            <p>{recipe.description}</p>
-          </CardContent>
-          <CardFooter className="flex justify-between">
-            <button>View Recipe</button>
-            {recipe.vegan && <p>Vegan!</p>}
-          </CardFooter>
-        </Card>
-      ))}
-    </div>
-  );
+    <main>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map(recipe => (
+          <Card key={recipe.id} className="flex flex-col justify-between">
+            <CardHeader className="flex-row gap-4 items-center">
+              <Avatar>
+                <AvatarImage src={`/img/${recipe.image}`} alt="@shadcn" />
+                <AvatarFallback>
+                  {recipe.title.slice(0,2)}
+                </AvatarFallback>
+              </Avatar>
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook.</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <Button>View Recipe</Button>
+              {recipe.vegan && <Badge variant="secondary">Vegan!</Badge>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
+  )
 }
